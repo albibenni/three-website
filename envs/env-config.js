56c567b582270608ad0a4a006561e4811177b9b7
Alberto Benatti <albi.benni8@gmail.com>
2023-08-17 18:12:59 +0200

    removed prev env var

--- envs/env-config.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-underscore-dangle
-window._env_ = {
-  VITE_APP_EMAILJS_SERVICE_ID:'service_14ahgac',
-  VITE_APP_EMAILJS_TEMPLATE_ID:'template_rrzhk0l',
-  VITE_APP_EMAILJS_PUBLIC_KEY:'EvWfF5ddosV2NBBY3',
-};
