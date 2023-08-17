486c72e720ddfcf20cd9d9c756c47f203cafef8b
Alberto Benatti <albi.benni8@gmail.com>
2023-08-17 17:25:35 +0200

    assets 1

---
+++ src/home/assets/index.ts
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import nextjs from './tech/nextjs.png';
+import java from './tech/java.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+
+import deda2 from './company/deda2.png';
+import intesa from './company/intesa.png';
+import forge from './company/forge.png';
+
+import treeWebsite from './tree-website.jpg';
+import coverage from './coverage.png';
+import exLibris from './exLibris.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  nextjs,
+  java,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  deda2,
+  intesa,
+  forge,
+  treeWebsite,
+  coverage,
+  exLibris,
+};
