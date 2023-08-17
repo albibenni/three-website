ba3f73e67061e5229f26d9d78fbd7aed9ca2c40c
Alberto Benatti <albi.benni8@gmail.com>
2023-08-17 17:41:37 +0200

    fixed tailwind route

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   mode: 'jit',
   theme: {
@@ -31,4 +27,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
